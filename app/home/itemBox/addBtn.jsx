73a1be406202f4cfe3609e4b6e10326344ba843f
CodeHub-Aki <codehub.aki@gmail.com>
2025-02-24 23:47:57 +0900

    モンスター追加ボタンにスタイルを追加

--- app/home/itemBox/addBtn.jsx
+++ app/home/itemBox/addBtn.jsx
@@ -2,9 +2,12 @@ import Image from "next/image";
 
 const Page = ({e}) => {
   return (
-    <div>
+    <div className="flex flex-col items-center w-fit">
       <Image src="/monsterIcon.png" alt="icon" width={100} height={93} />
-      <button onClick={e}>
+      <button
+        className="mt-2"
+        onClick={e}
+      >
         <Image src="/addButton.png" alt="Add Button" width={40} height={40} />
       </button>
     </div>
