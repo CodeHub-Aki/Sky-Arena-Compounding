e16bad599b918d90187f39c8e5ba0369ea86a68f
CodeHub-Aki <codehub.aki@gmail.com>
2025-03-11 02:30:34 +0900

    取得データを使用しグレートごとに情報を表示

--- app/home/itemBox/addBtn/addBtn.jsx
+++ app/home/itemBox/addBtn/addBtn.jsx
@@ -1,15 +1,25 @@
+import { useState } from "react";
 import Image from "next/image";
+import Menu from "./menu/menu"
 
-const Page = ({e}) => {
+const Page = ({monsterData}) => {
+
+  const [choiceMenu, setChoiceMenu] = useState(false)
+
+  const menuBtn = () => {
+    setChoiceMenu(!choiceMenu)
+  }
+
   return (
     <div className="flex flex-col items-center w-fit">
       <Image src="/monsterIcon.png" alt="icon" width={100} height={93} />
       <button
         className="mt-2"
-        onClick={e}
+        onClick={menuBtn}
       >
         <Image src="/addButton.png" alt="Add Button" width={40} height={40} />
       </button>
+      <Menu monsterData={monsterData}/>
     </div>
   );
 };
