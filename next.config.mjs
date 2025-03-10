fbf7538d127a2ed74ece837595d2a9c23a955a33
CodeHub-Aki <codehub.aki@gmail.com>
2025-03-11 02:27:21 +0900

    外部画像URL画像使用許可

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    domains: ['images.microcms-assets.io'], // microCMSの画像URLを許可
+  },
+};
 
 export default nextConfig;
