ff3ecb2f7f8c489118b801d8d3cd44250eeeefcb
CodeHub-Aki <codehub.aki@gmail.com>
2025-03-19 22:23:12 +0900

    選択したモンスターに応じたアイコン表示

--- app/home/fuse/components/monsterView.jsx
+++ app/home/fuse/components/monsterView.jsx
@@ -3,7 +3,7 @@ import { useState } from "react";
 
 import Menu from './menu'
 
-const Page = ({ monsterData, fuseData, setFuseData }) => {
+const Page = ({ monsterData, fuseData, setFuseData, addData, setAddData }) => {
 
   // モンスター選択画面の状態管理
   const [menuView, setMenuView] = useState(false)
@@ -14,26 +14,47 @@ const Page = ({ monsterData, fuseData, setFuseData }) => {
   }
 
   return (
-    <div className="flex flex-col items-center w-fit m-10">
+    <>
 
-      <Image src="/monsterIcon.png" alt="icon" width={100} height={93} priority/>
+      <div className="flex flex-col items-center w-fit m-10">
+        {addData.map((monster) => (
+              monster.icon && monster.icon.url ? (
+                <Image 
+                  key={monster.id} 
+                  src={monster.icon.url} 
+                  alt={monster.monster_name} 
+                  width={100} 
+                  height={93} 
+                  priority 
+                />
+              ) : null
+        ))}
+      </div>
 
-      <button
-        className="mt-2"
-        onClick={menuBtn}
-      >
-        <Image src="/addButton.png" alt="Add Button" width={40} height={40} />
-      </button>
+      <div className="flex flex-col items-center w-fit m-10">
 
-      {menuView && (
-        <Menu
-          monsterData={monsterData}
-          fuseData={fuseData}
-          serFuseData={setFuseData}
-        />
-      )}
+        <Image src="/monsterIcon.png" alt="icon" width={100} height={93} priority/>
 
-    </div>
+        <button
+          className="mt-2"
+          onClick={menuBtn}
+        >
+          <Image src="/addButton.png" alt="Add Button" width={40} height={40} />
+        </button>
+
+        {menuView && (
+          <Menu
+            monsterData={monsterData}
+            fuseData={fuseData}
+            setFuseData={setFuseData}
+            addData={addData}
+            setAddData={setAddData}
+          />
+        )}
+
+      </div>
+
+    </>
   );
 };
 
